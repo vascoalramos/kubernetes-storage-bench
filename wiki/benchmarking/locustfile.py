858cd6d1b57b3bbcc915fab4a4d087c5c2ff0ac6
daniel <danielregado@gmail.com>
2021-04-16 17:57:35 +0100

    Updated test locustfile

--- wiki/benchmarking/locustfile.py
+++ wiki/benchmarking/locustfile.py
@@ -5,12 +5,11 @@ import actions
 import blocks
 
 
-class ReadFromWiki(HttpUser):   
+class ReadFromWiki(HttpUser):
+
     def on_start(self):
         self.token = actions.login(self, "admin", "passwd")
         self.available_pages = blocks.getPageFullTree(self, 0)
-        blocks.deleteGeneratedAssets(self, self.token)
-        blocks.generatePage(self, self.token)
 
     @task
     def requestAllPages(self):
