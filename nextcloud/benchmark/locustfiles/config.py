ee06c8c760e1441823fb614c77b340ec9c5828e9
Vasco Ramos <vascoalramos@outlook.pt>
2021-04-29 18:13:01 +0100

    Update configs

--- nextcloud/benchmark/locustfiles/config.py
+++ nextcloud/benchmark/locustfiles/config.py
@@ -4,9 +4,7 @@ from locust import between, constant
 class StaticBenchmarkConfig:
     write_page_wait_seconds = constant(10)
     read_page_wait_seconds = constant(5)
-    media_instances_per_content_category = 3
 
 
 class UserSimulationBenchmarkConfig:
-    media_instances_per_content_category = 5
-    wait_time_between_tasks = between(5, 25)
+    wait_time_between_tasks = between(10, 30)
--- nextcloud/benchmark/locustfiles/static_write.py
+++ nextcloud/benchmark/locustfiles/static_write.py
@@ -1,6 +1,4 @@
 from locust import task, HttpUser
-
-# from locust.contrib.fasthttp import FastHttpUser
 from config import StaticBenchmarkConfig
 from datetime import datetime
 
@@ -10,22 +8,25 @@ import actions, random, os, json
 class StaticBenchmarkWrite(HttpUser):
     wait_time = StaticBenchmarkConfig.write_page_wait_seconds
     assets = []
-    files = []
-    folders = [""]
+    inserted_files = []
+    inserted_folders = [""]
 
     def on_start(self):
         with open("assets.json", "r") as file:
             self.assets = json.load(file)
 
     def on_stop(self):
-        inserted_assets = {"files": self.files, "folders": self.folders[1:]}
+        inserted_assets = {
+            "files": self.inserted_files,
+            "folders": self.inserted_folders[1:],
+        }
         with open("inserted_assets.json", "w") as file:
             json.dump(inserted_assets, file, indent=4, sort_keys=True)
 
     @task(20)
     def upload_file(self):
         filename = random.choice(self.assets)
-        folder = random.choice(self.folders)
+        folder = random.choice(self.inserted_folders)
         _, ext = os.path.splitext(filename)
 
         if folder == "":
@@ -39,10 +40,10 @@ class StaticBenchmarkWrite(HttpUser):
             if str(response.status_code)[0] != "2":
                 print(response.content)
 
-        self.files.append(new_filename)
+        self.inserted_files.append(new_filename)
 
     @task(1)
     def create_folder(self):
         new_foldername = f"_generated_folder{datetime.now().timestamp()}"
         actions.create_folder(self, new_foldername)
-        self.folders.append(new_foldername)
+        self.inserted_folders.append(new_foldername)
