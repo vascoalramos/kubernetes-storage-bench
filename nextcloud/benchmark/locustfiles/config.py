a976894c8f28478bc1f6ab2a3518f6f6e49e1471
Vasco Ramos <vascoalramos@outlook.pt>
2021-05-29 13:54:18 +0100

    Update config values

--- nextcloud/benchmark/locustfiles/config.py
+++ nextcloud/benchmark/locustfiles/config.py
@@ -2,9 +2,9 @@ from locust import between, constant
 
 
 class StaticBenchmarkConfig:
-    write_page_wait_seconds = constant(15)
+    write_page_wait_seconds = constant(10)
     read_page_wait_seconds = constant(5)
 
 
 class UserSimulationBenchmarkConfig:
-    wait_time_between_tasks = between(5, 20)
+    wait_time_between_tasks = between(5, 15)
