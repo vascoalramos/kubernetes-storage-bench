84001cd03dffc2c3317343de68ebd01886b197b0
Vasco Ramos <vascoalramos@outlook.pt>
2021-06-02 15:59:30 +0100

    Add nextcloud ceph static writes

--- nextcloud/benchmark/locustfiles/config.py
+++ nextcloud/benchmark/locustfiles/config.py
@@ -2,8 +2,8 @@ from locust import between, constant
 
 
 class StaticBenchmarkConfig:
-    write_page_wait_seconds = constant(7.5)
-    read_page_wait_seconds = constant(2.5)
+    write_page_wait_seconds = constant(10)
+    read_page_wait_seconds = constant(5)
 
 
 class UserSimulationBenchmarkConfig:
