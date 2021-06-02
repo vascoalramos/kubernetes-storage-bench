0196128c3104e6a2b6d838a99a8f0ce62657153c
Vasco Ramos <vascoalramos@outlook.pt>
2021-06-02 07:49:45 +0100

    Update monitor storage nodes

--- nextcloud/benchmark/locustfiles/config.py
+++ nextcloud/benchmark/locustfiles/config.py
@@ -2,9 +2,9 @@ from locust import between, constant
 
 
 class StaticBenchmarkConfig:
-    write_page_wait_seconds = constant(10)
-    read_page_wait_seconds = constant(5)
+    write_page_wait_seconds = constant(7.5)
+    read_page_wait_seconds = constant(2.5)
 
 
 class UserSimulationBenchmarkConfig:
-    wait_time_between_tasks = between(5, 15)
+    wait_time_between_tasks = between(5, 10)
